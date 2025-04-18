4a94dbbbfaad2852aacd477cd95a813d61a3bbeb
sanji <zero.0102003@gmail.com>
2025-04-18 22:23:59 +0330

    implement Playlist class.

--- src/musicPlayer/Playlist.java
+++ src/musicPlayer/Playlist.java
@@ -1,4 +1,102 @@
 package musicPlayer;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Playlist {
+    private String title;
+    private ArrayList<Music> playlist;
+    private User owner;
+
+    public Playlist(String title, User owner) {
+        this.owner = owner;
+        this.title = title;
+        playlist = new ArrayList<>();
+    }
+
+    public void addMusic(Music music, String password) {
+        if (!owner.getPassword().equals(password))
+            throw new InvalidOperationException("Invalid password");
+        if (playlist.contains(music))
+            throw new InvalidOperationException("Duplicate music");
+        playlist.add(music);
+    }
+
+    public void removeMusic(Music music, String password) {
+        if (!owner.getPassword().equals(password))
+            throw new InvalidOperationException("Invalid password");
+        if (!playlist.contains(music))
+            throw new InvalidOperationException("music not exist");
+        playlist.remove(music);
+    }
+
+    public void editTitle(String title, String password) {
+        if (!owner.getPassword().equals(password))
+            throw new InvalidOperationException("Invalid password");
+        if (title.equals(this.title))
+            throw new InvalidOperationException("repetitive title");
+        this.title = title;
+    }
+
+    public ArrayList<Music> searchInPlaylist(String title) {
+        ArrayList<Music> musicList = new ArrayList<Music>();
+        for (Music music : playlist) {
+            if (music.getTitle().equals(title)) {
+                musicList.add(music);
+            }
+        }
+        if (musicList.isEmpty())
+            return null;
+        else
+            return musicList;
+    }
+
+    public ArrayList<Music> searchInPlaylist(String title, User singer) {
+        ArrayList<Music> musicList = new ArrayList<Music>();
+        for (Music music : playlist) {
+            if (music.getTitle().equals(title) && music.getSinger().getUsername().equals(singer.getUsername())) {
+                musicList.add(music);
+            }
+        }
+        if (musicList.isEmpty())
+            return null;
+        else
+            return musicList;
+    }
+
+    public void playPlaylist() {
+        for (Music music : playlist) {
+            music.play();
+        }
+    }
+
+    public void shuffelPlay() {
+        Random rand = new Random();
+        int index = rand.nextInt(playlist.size());
+        playlist.get(index).play();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<Music> getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(ArrayList<Music> playlist) {
+        this.playlist = playlist;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
 }
