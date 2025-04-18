87206e6bd75cc9b378eaca72c9090143d198ad0e
sanji <zero.0102003@gmail.com>
2025-04-18 23:30:49 +0330

    test the project.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,93 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import musicPlayer.*;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // create Users
+        User matin = null, pariya = null, mina = null;
+        try {
+            matin = new User("Sanji", "matin1385");
+            pariya = new User("cat", "pariya1384");
+            mina = new User("mina chibi", "mina");
+        } catch (InvalidOperationException e) {
+            System.out.println(e.getMessage());
         }
+
+        if (!Objects.isNull(matin)) {
+            matin.follow(pariya);
+            System.out.println("matin follower list: " + matin.getFollowerList().getFirst());
+        }
+
+        Music sucker = null, sholeshKon = null, song = null;
+        try {
+            sucker = new Music("Sucker", new User("Marcus king", "123456789"));
+            sholeshKon = new Music("sholesh kon", new User("Shaye", "12346578"));
+            song = new Music("Sucker", pariya);
+        } catch (InvalidOperationException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Search for sucker: ");
+        ArrayList<Music> result1 = Music.search("Sucker");
+        try {
+            for (Music music : result1)
+                System.out.println(music.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Search for sucker with singer 'pariya' : ");
+        ArrayList<Music> result2 = Music.search("Sucker", pariya);
+        try {
+            for (Music music : result2)
+                System.out.println(music.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            matin.playMusic(sucker);
+            matin.playMusic(sholeshKon);
+            matin.playMusic(sucker);
+            System.out.println("Stream count for song1: " + sucker.getNumberOfStream());
+            RegularBehavior test =(RegularBehavior) matin.getBehavior();
+            System.out.println("playing limit for matin is :" + test.getPlayingLimit());
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+        }
+
+        matin.buyPremium(matin, 4);
+        matin.creatPlayList("Favorites", matin);
+        for (int i = 0; i < 10; i++) {
+            matin.playMusic(sholeshKon);
+        }
+        System.out.println("Stream count for song1: " + sholeshKon.getNumberOfStream());
+
+        Playlist playlist = matin.getPlaylists().getFirst();
+        playlist.addMusic(sucker, "matin1385");
+        playlist.addMusic(sholeshKon, "matin1385");
+
+        try {
+            playlist.addMusic(sucker, "matin1385");
+        } catch (InvalidOperationException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            playlist.addMusic(song, "wrongpass");
+        } catch (InvalidOperationException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("search in playlist for sucker: ");
+        ArrayList<Music> test1 = matin.getPlaylists().getFirst().searchInPlaylist("Sucker");
+        for (Music music : test1)
+            System.out.println(music.toString());
+
+        Playlist playlist1 = matin.getPlaylists().getFirst();
+        playlist1.playPlaylist();
+        playlist1.shuffelPlay();
     }
 }
