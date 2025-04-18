8f696527251de9faae79478f8dd9fc7f7fac310f
sanji <zero.0102003@gmail.com>
2025-04-18 22:25:09 +0330

    implement RegularBehavior class.

--- src/musicPlayer/RegularBehavior.java
+++ src/musicPlayer/RegularBehavior.java
@@ -1,4 +1,24 @@
 package musicPlayer;
 
-public class RegularBehavior {
+public class RegularBehavior implements UserBehavior {
+    private int playingLimit = 5;
+
+    @Override
+    public void createPlaylist(String Title, User Owner) {
+        throw new InvalidOperationException("You are not allowed to creat play list.");
+    }
+
+    @Override
+    public void playMusic(Music music) {
+        if (playingLimit <= 0) {
+            throw new InvalidOperationException("You are not allowed to play anything.");
+        }
+        music.play();
+        playingLimit--;
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+        owner.setBehavior(new PremiumBehavior(month));
+    }
 }
